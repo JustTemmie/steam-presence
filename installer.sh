c8d653aa50a3d85a334416c2d7185b22b25c08ab
Radical <66682497+Radiicall@users.noreply.github.com>
2023-05-28 17:47:18 +0200

    Fix up this horrible script
    Use current directory instead of symlinking everything horribly to the "correct" places, this should now work *better* hopefully.... I had to fix this when i saw what it did

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "This script is LINUX ONLY, and it's still in testing phases, if you encounter any bugs please open an issue"
 read -p "Press the enter key to continue..."
@@ -6,7 +6,7 @@ echo ""
 
 
 # Check if script is running as root
-if [ "$(id -u)" == "0" ]; then
+if [ `whoami` == "root" ]; then
    echo "This script cannot be run as root. Please run as a regular user"
    exit 1
 fi
@@ -18,13 +18,6 @@ if [ ! -f "config.json" ]; then
    ls
    exit 1
 fi
-
-# Create steam-presence folder if it does not exist
-if [ ! -d "$HOME/steam-presence" ]; then
-  mkdir -p "$HOME/steam-presence"
-fi
-
-echo "Creating symlinks of necessary files..."
 
 # Create games.txt if it doesn't exist
 if [ ! -e "$(pwd)/games.txt" ]; then
@@ -44,24 +37,7 @@ if [ ! -e "$(pwd)/customGameIDs.json" ]; then
   echo "{}" > "$(pwd)/customGameIDs.json"
 fi
 
-
-# Create symlinks of necessary files
-ln -s "$(pwd)/main.py" "$HOME/steam-presence/main.py"
-ln -s "$(pwd)/requirements.txt" "$HOME/steam-presence/requirements.txt"
-ln -s "$(pwd)/steam-presence.service" "$HOME/steam-presence/steam-presence.service"
-ln -s "$(pwd)/config.json" "$HOME/steam-presence/config.json"
-ln -s "$(pwd)/games.txt" "$HOME/steam-presence/games.txt"
-ln -s "$(pwd)/icons.txt" "$HOME/steam-presence/icons.txt"
-ln -s "$(pwd)/customGameIDs.json" "$HOME/steam-presence/customGameIDs.json"
-
-echo "Symlinks created."
-
-# Change to steam-presence directory
-cd "$HOME/steam-presence"
-
-echo "Executing commands in steam-presence directory..."
-
-# Execute commands in steam-presence directory
+echo "Setting up virtual environment"
 python3 -m venv .
 ./bin/python -m pip install --upgrade pip wheel
 ./bin/python -m pip install -r requirements.txt
@@ -75,13 +51,14 @@ timeout 2.5s ./bin/python ./main.py
 echo "Test might've worked, did it spit out any errors?"
 echo "Commands executed."
 
-# Replace "deck" user with local username in steam-presence.service file
-echo "Editing service file"
-local_username=$(whoami)
-sed -i "s/deck/$local_username/g" "$HOME/steam-presence/steam-presence.service"
+# Replace "/home/deck" with current directory in steam-presence.service file
+echo "Setting up service file"
+mkdir -p "$HOME/.config/systemd/user"
+sed -e "s~/home/deck/steam-presence~$PWD~g" "$PWD/steam-presence.service" > $HOME/.config/systemd/user/steam-presence.service
 
 echo "Starting service"
-systemctl --user --now enable "$HOME/steam-presence/steam-presence.service"
+systemctl --user daemon-reload
+systemctl --user --now enable "steam-presence.service"
 
 echo "If you encountered any errors with this script, please create an issue on the GitHub page"
 echo ""
