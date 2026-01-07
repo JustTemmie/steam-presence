261fe54cdac2abb3b0a2c70b4729e0416636a52b
Twig <git@beaver.mom>
2026-01-07 19:08:00 +0100

    add ability to upload to catbox

---
+++ src/apis/catbox.py
@@ -0,0 +1,48 @@
+import requests
+import threading
+
+from typing import Optional, Literal
+import logging
+
+# locks to ensure we're not uploading multiple copies of the same file
+_uploading_lock = threading.Lock()
+_uploading_set = set()
+
+def upload_file(
+    file_path: str,
+    ttl: Literal['1h', '12h', '24h', '72h', 'indefinite'] = None
+) -> Optional[str]:
+    with _uploading_lock:
+        if file_path in _uploading_set:
+            return None
+        _uploading_set.add(file_path)
+
+    if ttl == "indefinite":
+        url = "https://catbox.moe/user/api.php"
+        data = {
+            "reqtype": "fileupload"
+        }
+    else:
+        url = "https://litterbox.catbox.moe/resources/internals/api.php"
+        data = {
+            "reqtype": "fileupload",
+            "time": ttl
+        }
+
+    try:
+        with open(file_path, "rb") as f:
+            files = {"fileToUpload": (file_path, f)}
+            logging.debug("uploading %s to catbox", file_path)
+            response = requests.post(url, data=data, files=files, timeout=120)
+            logging.debug("finished uploading %s to catbox", file_path)
+
+        response.raise_for_status()
+        return response.text
+
+    except requests.exceptions.RequestException:
+        logging.error("catbox upload failed: %s", file_path)
+        return None
+
+    finally:
+        with _uploading_lock:
+            _uploading_set.discard(file_path)
