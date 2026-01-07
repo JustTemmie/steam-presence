434129d850ebd82b119e34434d6c771244582498
Twig <git@beaver.mom>
2026-01-07 19:07:56 +0100

    implement disk caching

---
+++ src/presence_manager/disk_cache.py
@@ -0,0 +1,54 @@
+import os
+import time
+import json
+
+from dataclasses import dataclass
+from typing import Optional
+import threading
+
+DISK_LOCK = threading.RLock()
+
+def cache_fetch(bank: str, key: str, ttl: float) -> Optional[dict]:
+    with DISK_LOCK:
+        os.makedirs("cache", exist_ok=True)
+
+        bank = f"cache/banks/{bank}.json"
+        cache: dict = {}
+
+        if os.path.exists(bank):
+            with open(bank, 'r', encoding = "utf-8") as f:
+                cache = json.load(f)
+
+        fetched = cache.get(key)
+        if not fetched:
+            return
+
+        if fetched.get("last_update", 0) > time.time() - ttl:
+            return fetched.get("value")
+        
+        else:
+            cache.pop(key)
+            with open(bank, 'w', encoding="utf-8") as f:
+                json.dump(cache, f)
+
+def cache_store(bank: str, key: str, value: dict):
+    with DISK_LOCK:
+        os.makedirs("cache", exist_ok=True)
+
+        path = "cache/banks"
+        bank = f"cache/banks/{bank}.json"
+        cache: dict = {}
+
+        os.makedirs(path, exist_ok=True)
+
+        if os.path.exists(bank):
+            with open(bank, 'r', encoding="utf-8") as f:
+                cache = json.load(f)
+        
+        cache[key] = {
+            "value": value,
+            "last_update": time.time()
+        }
+
+        with open(bank, 'w', encoding="utf-8") as f:
+            json.dump(cache, f)
