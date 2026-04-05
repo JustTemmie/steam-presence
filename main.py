bd7c54d3f65948bef8b689dfcc14150e1905a18b
Twig <git@beaver.mom>
2026-04-05 17:28:08 +0200

    version bump

--- main.py
+++ main.py
@@ -1011,7 +1011,7 @@ def checkForUpdate():
 def main():
     global currentVersion
     # this always has to match the newest release tag
-    currentVersion = "v1.12.3"
+    currentVersion = "v1.12.4"
     
     # check if there's any updates for the program
     checkForUpdate()
